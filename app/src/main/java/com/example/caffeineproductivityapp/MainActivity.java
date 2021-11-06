fda3ead6f48bcb3c87117d6c4c330b7a097248b3
SamuelKellyDev <waterdrinker76@gmail.com>
2021-11-05 20:52:27 -0500

    Added deleteButton along with code for deleting rows, having trouble getting it to work past one row. Will continue debugging tomorrow.

--- app/src/main/java/com/example/caffeineproductivityapp/MainActivity.java
+++ app/src/main/java/com/example/caffeineproductivityapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.caffeineproductivityapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText amountInput;
     EditText timeInput;
     Button entryButton;
-    List<String> list = new ArrayList<String>();
+    List<int[]> list = new ArrayList<int[]>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,32 +42,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             tblRow.addView(i);
         tbl.addView(tblRow);
     }
-    // Sets text and the size of the text
-    public void textConfig(TextView amountText1, TextView timeText1, EditText amountText, EditText timeText, String amount, String time, int textSize) {
-        amountText1.setText("Amount: ");
-        timeText1.setText("Time: ");
+    // Sets text and the size of the text, BP means boilerplate
+    public void textConfig(TextView amountTextBP, TextView timeTextBP, EditText amountText, EditText timeText, String amount, String time, Button deleteButton, int textSize) {
+        amountTextBP.setText("Amount: ");
+        timeTextBP.setText("Time: ");
         amountText.setText(amount);
         timeText.setText(time);
-        amountText1.setTextSize(textSize);
-        timeText1.setTextSize(textSize);
+        deleteButton.setText("Delete");
+        amountTextBP.setTextSize(textSize);
+        timeTextBP.setTextSize(textSize);
         amountText.setTextSize(textSize);
         timeText.setTextSize(textSize);
+        deleteButton.setTextSize(textSize);
     }
-    // This method gets called everytime a button is clicked
+    // This method gets called everytime a button is clicked, BP means boilerplate
     public void onClick(View view) {
+        setContentView(R.layout.activity_main);
         TableRow tblRow1 = new TableRow(this);
-        TextView amountText1 = new TextView(this);
-        TextView timeText1 = new TextView(this);
+        TextView amountTextBP = new TextView(this);
+        TextView timeTextBP = new TextView(this);
         EditText amountText = new EditText(this);
         EditText timeText = new EditText(this);
-        amount = amountInput.getText().toString();
-        time = timeInput.getText().toString();
-        list.add(amount + "," + time);
         if (view.getId() == R.id.entryButton) {
-            TableLayout tbl1 = (TableLayout) findViewById(R.id.tbl1);
-            textConfig(amountText1, timeText1, amountText, timeText, amount, time, 25);
-            View views[] = {amountText1, amountText, timeText1, timeText};
+            Button deleteButton = new Button(this);
+            deleteButton.setTag("deleteButton");
+            TableLayout tbl1 = findViewById(R.id.tbl1);
+            textConfig(amountTextBP, timeTextBP, amountText, timeText, amountInput.getText().toString(), timeInput.getText().toString(), deleteButton, 25);
+            View views[] = {deleteButton, amountTextBP, amountText, timeTextBP, timeText};
             addViewsToTable(views, tblRow1, tbl1);
+            int[] arrayNums = {deleteButton.getId(), tblRow1.getId()};
+            list.add(arrayNums);
+            deleteButton.setOnClickListener(this);
+        }
+        else if (view.getTag() == "deleteButton") {
+            TableLayout tbl1 = findViewById(R.id.tbl1);
+            for (int[] i: list) {
+                if (i[0] == view.getId())
+                    tbl1.removeView(findViewById(i[1]));
+            }
         }
     }
     /*
