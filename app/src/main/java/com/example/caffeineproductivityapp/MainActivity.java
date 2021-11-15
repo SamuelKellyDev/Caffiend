d916171238edb5b511591201db50d667a26c19fb
SamuelKellyDev <waterdrinker76@gmail.com>
2021-11-15 01:28:05 -0600

    Fixed sort functionality so rows of user data automatically sort in descending order Cleaned up some of the code to make it easier to read

--- app/src/main/java/com/example/caffeineproductivityapp/MainActivity.java
+++ app/src/main/java/com/example/caffeineproductivityapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.caffeineproductivityapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,19 +16,27 @@ import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.data.Entry;
 
 import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     String amount, time;
     EditText amountInput;
-    EditText timeInput;
+    Button timeInput;
     Button entryButton;
     Button sideButton;
+    int hour, minute;
     List<int[]> IDlist = new ArrayList<>();
     static List<String[]> dataList = new ArrayList<>();
     @Override
@@ -40,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         entryButton = findViewById(R.id.entryButton);
         entryButton.setOnClickListener(this);
         sideButton.setOnClickListener(this);
+        timeInput.setOnClickListener(this);
     }
     // Combines view addition process to table into one method that takes all needed views in as parameters
     public void addViewsToTable(View[] views, TableRow tblRow, TableLayout tbl) {
@@ -58,26 +70,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     // This method gets called everytime a button is clicked, BP means boilerplate
     public void onClick(View view) {
-        amountInput.setHint("ex: 23 mg");
-        timeInput.setHint("ex: 2:00 PM");
-        TableRow tblRow1 = new TableRow(this);
-        TextView amountText = new TextView(this);
-        TextView timeText = new TextView(this);
-        if (view.getId() == R.id.entryButton) {
+        if ((amountInput.getText().toString().isEmpty() || timeInput.getText().toString().isEmpty()) && view.getId() == R.id.entryButton) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Input cannot be empty", 23);
+            toast.show();
+            return;
+        }
+        else if (view.getId() == R.id.entryButton) {
+            TableRow tblRow1 = new TableRow(this);
+            TextView amountText = new TextView(this);
+            TextView timeText = new TextView(this);
             Button deleteButton = new Button(this);
-            deleteButton.setTag("deleteButton");
-            TableLayout tbl1 = findViewById(R.id.tbl1);
-            textConfig(amountText, timeText, amountInput.getText().toString(), timeInput.getText().toString(), deleteButton, 25);
-            String[] dataNums = {amountInput.getText().toString(), timeInput.getText().toString()};
-            dataList.add(dataNums);
-            View views[] = {deleteButton, amountText, timeText};
-            addViewsToTable(views, tblRow1, tbl1);
             deleteButton.setId(View.generateViewId()); // had to set generated id's bc the auto id is just the exact same so delete remove wasn't working
             tblRow1.setId(View.generateViewId());
-            int[] arrayNums = {deleteButton.getId(), tblRow1.getId()};
-            IDlist.add(arrayNums);
+            timeText.setId(View.generateViewId());
+            amountText.setId(View.generateViewId());
+            TableLayout tbl1 = findViewById(R.id.tbl1);
+            textConfig(amountText, timeText, amountInput.getText().toString(), timeInput.getText().toString(), deleteButton, 25);
+            dataList.add(new String[]{amountInput.getText().toString(), timeInput.getText().toString(), String.valueOf(amountText.getId()), String.valueOf(timeText.getId())});
+            addViewsToTable(new View[]{deleteButton, amountText, timeText}, tblRow1, tbl1);
+            IDlist.add(new int[]{deleteButton.getId(), tblRow1.getId()});
+            // replaces all text besides numbers with nothing to make for easier processing of user provided data
+            for (String[] i: dataList) {
+                for (int j = 0; j < i.length; j++) {
+                    i[j] = i[j].replaceAll("[^0-9]", "");
+                }
+            }
+            if (dataList.size() > 1)
+                sort(dataList);
             deleteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
+                // Removes views and data when delete button is clicked by searching through the stored data of views
                 public void onClick(View view) {
                     for (int[] i: IDlist) {
                         if (i[0] == view.getId()) {
@@ -85,6 +107,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             tbl1.removeView(findViewById(i[1]));
                             dataList.remove(IDlist.indexOf(i));
                             IDlist.remove(i);
+                            if (dataList.size() > 1)
+                                sort(dataList);
                             break;
                         }
                     }
@@ -95,10 +119,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent i = new Intent(getApplicationContext(), MainActivity2.class);
             startActivity(i);
         }
+        else if (view.getId() == R.id.timeInput) {
+            TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    hour = selectedHour;
+                    minute = selectedMinute;
+                    timeInput.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+                }
+            };
+
+            int style = AlertDialog.THEME_HOLO_DARK;
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, true);
+
+            timePickerDialog.setTitle("Select Time");
+            timePickerDialog.show();
+        }
     }
-    /*
-    public int[] sort() {
-        return;
+    // This method sorts data from smallest to largest so graph displays correctly and makes rows in correct order as well
+    public void sort(List<String[]> dataList) {
+        for (int i = 0; i < dataList.size(); i++) {
+            for (int j = i + 1; j < dataList.size(); j++) {
+                if (Float.parseFloat(dataList.get(i)[1]) > Float.parseFloat(dataList.get(j)[1])) {
+                    TextView amountText1 = findViewById(Integer.parseInt(dataList.get(i)[2]));
+                    TextView timeText1 = findViewById(Integer.parseInt(dataList.get(i)[3]));
+                    TextView amountText2 = findViewById(Integer.parseInt(dataList.get(j)[2]));
+                    TextView timeText2 = findViewById(Integer.parseInt(dataList.get(j)[3]));
+                    String[] valueSave = {amountText1.getText().toString(), timeText1.getText().toString()}; // stores data before switch
+                    amountText1.setText(amountText2.getText().toString());
+                    timeText1.setText(timeText2.getText().toString());
+                    amountText2.setText(valueSave[0]);
+                    timeText2.setText(valueSave[1]);
+                    String[] iData = dataList.get(i); // stores data before switch
+                    dataList.set(i, dataList.get(j));
+                    dataList.set(j, iData);
+                    break;
+                }
+            }
+        }
     }
-     */
+
 }
--- app/src/main/java/com/example/caffeineproductivityapp/MainActivity2.java
+++ app/src/main/java/com/example/caffeineproductivityapp/MainActivity2.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -36,30 +37,51 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         entriesButton.setOnClickListener(this);
         lineChart = (LineChart) findViewById(R.id.lineChart);
         List<Entry> entries = new ArrayList<Entry>();
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+        lineChart.setMaxVisibleValueCount(2);
 
         for (String[] i: dataList) {
-            for (String j: i) {
-                j = j.replaceAll("[^0-9]", "");
-            }
             entries.add(new Entry(Float.parseFloat(i[1]), Float.parseFloat(i[0])));
         }
 
         LineDataSet dataSet = new LineDataSet(entries, "Label");
         dataSet.setColor(2);
         dataSet.setValueTextColor(2);
+        dataSet.setFillAlpha(110);
 
         LineData lineData = new LineData(dataSet);
         lineChart.setData(lineData);
         lineChart.invalidate();
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.RED);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(true);
+        xAxis.setGranularity(1f);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        YAxis rightAxis = lineChart.getAxisRight();
+        YAxis yAxis = lineChart.getAxis(YAxis.AxisDependency.LEFT);
+        yAxis.setTextSize(10f);
+        yAxis.setTextColor(Color.RED);
+        yAxis.setDrawAxisLine(true);
+        yAxis.setDrawGridLines(true);
+        yAxis.setGranularity(1f);
+
         ValueFormatter formatter = new ValueFormatter() {
             @Override
             public String getAxisLabel(float value, AxisBase axis) {
-                return "Time: " + value;
+                if (axis == xAxis)
+                    return "Time: " + value;
+                else
+                    return "Amount: " + value;
             }
         };
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setGranularity(1f);
         xAxis.setValueFormatter(formatter);
+        yAxis.setValueFormatter(formatter);
     }
 
     public void onClick(View view) {
